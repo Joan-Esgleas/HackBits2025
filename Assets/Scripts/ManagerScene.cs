8b56ef5c0e3439213d3d8cc89efdb5a853419c24
erikgauchia <erikgauchia@gmail.com>
2025-12-13 17:57:52 +0100

    muchas cosas

--- Assets/Scripts/ManagerScene.cs
+++ Assets/Scripts/ManagerScene.cs
@@ -1,56 +1,119 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
+
+[System.Serializable]
+public class Respuesta
+{
+    public string[] respuestasEsperadas;
+    public TMP_InputField inputField;
+}
 
 public class ManagerScene : MonoBehaviour
 {    
     [Header("Scene Settings")]
-
+    [SerializeField]
     private GameObject sysmanagerPrefab;
     private SysManager sysmanager;
-    
+
+    [Space (10)]
+    [Header("Input Field")]
+    [SerializeField]
+    TMP_InputField dia_inputField;
+    [SerializeField]
+    TMP_InputField mes_inputField;
+    [SerializeField]
+    TMP_InputField ano_inputField;
+
+
+    [Space(10)]
+    [Header("Respuesta Settings")]
+    [SerializeField]
+    List<Respuesta> respuestas;
+
     private void Start()
     {
        sysmanager = FindAnyObjectByType<SysManager>();
-        if(managerGame == null) {
+        if(sysmanager == null) {
             GameObject.Instantiate(sysmanagerPrefab);
-            managerGame = sysmanagerPrefab.GetComponent<SysManager>();
+            sysmanager = sysmanagerPrefab.GetComponent<SysManager>();
+        }
+        if (dia_inputField != null) dia_inputField.onSubmit.AddListener(delegate { GetDia(); });
+        if (mes_inputField != null) mes_inputField.onSubmit.AddListener(delegate { GetMes(); });
+        if (ano_inputField != null) ano_inputField.onSubmit.AddListener(delegate { GetAno(); });
+        foreach (Respuesta r in respuestas) {
+            r.inputField.onSubmit.AddListener(delegate { CheckRespuesta(r); });
         }
     }
 
     public void LoadScene(string sceneName) {
         SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
     }
-    
-    public void checkDiaCumpleanos(int d) {
-      if(d == sysmanager.usuario.dia_nacimiento)
-        sysmanager.puntuacion++;
+    bool checkDay(string s)
+    {
+        DateTime dt = DateTime.Now;
+        string day_eng = dt.DayOfWeek.ToString();
+        if (s == "Lunes" && day_eng == "Monday") return true;
+        if (s == "Martes" && day_eng == "Tuesday") return true;
+        if (s == "Miércoles" && day_eng == "Wednesday") return true;
+        if (s == "Jueves" && day_eng == "Thursday") return true;
+        if (s == "Viernes" && day_eng == "Friday") return true;
+        if (s == "Sábado" && day_eng == "Saturday") return true;
+        if (s == "Domingo" && day_eng == "Sunday") return true;
+        return false;
+
+    }
+    public void GetDia() {
+        string s = dia_inputField.text;
+        if (checkDay(s))
+        {
+            AddPuntuacion();
+        }
     }
 
-    public void checkMesCumpleanos(int m) {
-      if(m == sysmanager.usuario.mes_nacimiento)
-        sysmanager.puntuacion++;
+    public void GetMes() {
+        string d_str = mes_inputField.text;
+        int d = int.Parse(d_str);
+        if(d.ToString() == DateTime.Now.Month.ToString()) AddPuntuacion();
+
     }
 
-    public void checkAnoCumpleanos(int a) {
-      if(a == sysmanager.usuario.ano_naciemiento)
-        sysmanager.puntuacion++;
+    public void GetAno() {
+        string d_str = ano_inputField.text;
+        int d = int.Parse(d_str);
+        if (d.ToString() == DateTime.Now.Year.ToString()) AddPuntuacion();
     }
 
-    public void checkNombre(string n) {
-      if(n == sysmanager.usuario.nombre)
-        sysmanager.puntuacion++;
+    public void CheckRespuesta(Respuesta r)
+    {
+        foreach(string respuestaE in r.respuestasEsperadas)
+        {
+            if (r.inputField.text == respuestaE)
+            {
+                AddPuntuacion();            }
+        }
+       
+    }
+    public void AddPuntuacion()
+    {
+        sysmanager.usuario.puntuacion++;
     }
 
-    public void checkNombreCiudad(string c) {
-      if(n == sysmanager.usuario.ciudad_nacimeinto)
-        sysmanager.puntuacion++;
+    public void AddPuntuacion(int n)
+    {
+        sysmanager.usuario.puntuacion += n;
     }
+
 
     private void Update()
     {
+
     }
     
 }
--- Assets/Scripts/SysManager.cs
+++ Assets/Scripts/SysManager.cs
@@ -5,12 +5,8 @@ using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public struct Usuario {
-  string nombre;
-  int dia_nacimiento;
-  int mes_nacimiento;
-  int ano_naciemiento;
-  string ciudad_nacimeinto;
-  int puntuacion;
+
+  public int puntuacion;
 }
 
 public class SysManager : MonoBehaviour
@@ -27,11 +23,6 @@ public class SysManager : MonoBehaviour
         
         Instance = this;
         DontDestroyOnLoad(gameObject);
-        usuario.nombre = "";
-        usuario.dia_nacimiento = -1;
-        usuario.mes_nacimiento = -1;
-        usuario.ano_naciemiento = -1;
-        usuario.ciudad_nacimeinto ="";
         usuario.puntuacion = 0;
     }
 }
